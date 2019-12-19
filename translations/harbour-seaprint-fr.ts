3dd4db9aaac99309eaface44d0b7db532d56567f
ensag-dev <59049879+ensag-dev@users.noreply.github.com>
2019-12-19 10:59:29 +0100

    Creation of the French locale

---
+++ translations/harbour-seaprint-fr.ts
@@ -0,0 +1,174 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AddPrinterDialog</name>
+    <message>
+        <source>Add printer</source>
+        <translation>Ajouter une imprimante</translation>
+    </message>
+    <message>
+        <source>On WiFI:</source>
+        <translation>En Wifi :</translation>
+    </message>
+    <message>
+        <source>Found:</source>
+        <translation>Trouvées :</translation>
+    </message>
+    <message>
+        <source>No printer found</source>
+        <translation>Aucune imprimante détectée</translation>
+    </message>
+</context>
+<context>
+    <name>ChoiceSetting</name>
+    <message>
+        <source>draft</source>
+        <translation>brouillon</translation>
+    </message>
+    <message>
+        <source>normal</source>
+        <translation>normale</translation>
+    </message>
+    <message>
+        <source>high</source>
+        <translation>haute</translation>
+    </message>
+    <message>
+        <source>unknown quality </source>
+        <translation>qualité inconnue</translation>
+    </message>
+    <message>
+        <source>portrait</source>
+        <translation>portrait</translation>
+    </message>
+    <message>
+        <source>landscape</source>
+        <translation>paysage</translation>
+    </message>
+    <message>
+        <source>reverse landscape</source>
+        <translation>paysage inversé</translation>
+    </message>
+    <message>
+        <source>reverse portrait</source>
+        <translation>portrait inversé</translation>
+    </message>
+    <message>
+        <source>unknown orientation </source>
+        <translation>orientation inconnue</translation>
+    </message>
+    <message>
+        <source>dpi</source>
+        <translation>dpi</translation>
+    </message>
+    <message>
+        <source>dots/cm</source>
+        <translation>pts/cm</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>SeaPrint</source>
+        <translation>SeaPrint</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Add by URL</source>
+        <translation>Ajouter par URL</translation>
+    </message>
+    <message>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualiser</translation>
+    </message>
+    <message>
+        <source>No file selected</source>
+        <translation>Aucun fichier sélectionné</translation>
+    </message>
+    <message>
+        <source>Choose file</source>
+        <translation>Choisir un fichier</translation>
+    </message>
+    <message>
+        <source>None</source>
+        <translation>Aucun</translation>
+    </message>
+    <message>
+        <source>View jobs</source>
+        <translation>Voir les travaux</translation>
+    </message>
+    <message>
+        <source>Remove printer</source>
+        <translation>Supprimer l'imprimante</translation>
+    </message>
+    <message>
+        <source>Unknown</source>
+        <translation>Inconnu</translation>
+    </message>
+    <message>
+        <source>Removing printer</source>
+        <translation>Suppression de l'imprimante</translation>
+    </message>
+</context>
+<context>
+    <name>JobsPage</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Actualiser</translation>
+    </message>
+    <message>
+        <source>jobs</source>
+        <translation>travaux</translation>
+    </message>
+    <message>
+        <source>Untitled job</source>
+        <translation>Tâche sans nom</translation>
+    </message>
+    <message>
+        <source>Cancel job</source>
+        <translation>Annuler la tâche</translation>
+    </message>
+    <message>
+        <source>Cancelling job</source>
+        <translation>Annulation de la tâche</translation>
+    </message>
+    <message>
+        <source>job</source>
+        <translation>tâche</translation>
+    </message>
+</context>
+<context>
+    <name>PrinterPage</name>
+    <message>
+        <source>Print</source>
+        <translation>Imprimer</translation>
+    </message>
+    <message>
+        <source>Sides</source>
+        <translation>Côté</translation>
+    </message>
+    <message>
+        <source>Copies</source>
+        <translation>Copies</translation>
+    </message>
+    <message>
+        <source>Color mode</source>
+        <translation>Mode couleur</translation>
+    </message>
+    <message>
+        <source>Quality</source>
+        <translation>Qualité</translation>
+    </message>
+    <message>
+        <source>Resolution</source>
+        <translation>Résolution</translation>
+    </message>
+</context>
+</TS>
