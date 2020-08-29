fa34843c3e9c693b78041eb81405482ebad3fd27
Anton Thomasson <antonthomasson@gmail.com>
2020-08-29 17:26:26 +0200

    Update ppm2pwg/Bytestream
    ...to get the size constructor back.

--- src/ippdiscovery.cpp
+++ src/ippdiscovery.cpp
@@ -146,7 +146,7 @@ void IppDiscovery::readPendingDatagrams()
     while (socket->hasPendingDatagrams()) {
 
         size_t size = socket->pendingDatagramSize();
-        Bytestream resp = Bytestream::preallocated(size);
+        Bytestream resp(size);
         QHostAddress sender;
         quint16 senderPort;
 
